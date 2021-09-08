d70a97ad3c1b44dbdf2bb3090f76bfbeba6890dd
William Firmino <williamwf07@gmail.com>
2021-09-08 13:50:18 -0300

    Copiado script e css para o form do dia 4

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,33 @@
-import './style.css'
+import "./style.css";
+
+const form = document.querySelector('[data-js="carForm"]');
+const table = document.querySelector('[data-js="carTableBody"]');
+
+const getFormElement = (event) => (elementName) => {
+  return event.target.elements[elementName];
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const getElement = getFormElement(event);
+  const image = getElement("image");
+  const model = getElement("model");
+  const year = getElement("year");
+  const plate = getElement("plate");
+  const color = getElement("color");
+
+  const elements = [image, model, year, plate, color];
+
+  const tr = document.createElement("tr");
+
+  elements.forEach((element) => {
+    const td = document.createElement("td");
+    td.textContent = element.value;
+    tr.appendChild(td);
+  });
+
+  table.appendChild(tr);
+
+  form.reset();
+  image.focus();
+});
