72c2dee981054e0da266d5e2c15bb8a26befc350
William Firmino <williamwf07@gmail.com>
2021-10-12 01:25:37 -0300

    Exercício 3 - Cadastro do carro no servidor usando POST

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -8,38 +8,69 @@ const getFormElement = (event) => (elementName) => {
   return event.target.elements[elementName];
 };
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const getElement = getFormElement(event);
-  const image = getElement("image");
-  const brandModel = getElement("model");
-  const year = getElement("year");
-  const plate = getElement("plate");
-  const color = getElement("color");
 
-  const elements = [image, brandModel, year, plate, color];
+  const data = {
+    image: getElement("image").value,
+    brandModel: getElement("model").value,
+    year: getElement("year").value,
+    plate: getElement("plate").value,
+    color: getElement("color").value,
+  };
 
-  createTableRow(elements);
+  const result = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .catch((err) => {
+      ({ error: true, message: err.message });
+    });
 
   form.reset();
   image.focus();
+
+  if (result.error) {
+    console.log("Erro ao cadastrar:", result.message);
+    return;
+  }
+
+  console.log(result);
+
+  createTableRow(data);
 });
 
-function createTableRow(carElements) {
+function createTableRow(data) {
   if (table.querySelector(".noCars")) {
     table.innerHTML = "";
   }
+
+  const carElements = [
+    { type: "image", value: { src: data.image, alt: data.brandModel } },
+    { type: "string", value: data.brandModel },
+    { type: "string", value: data.year },
+    { type: "string", value: data.plate },
+    { type: "color", value: data.color },
+  ];
 
   const tr = document.createElement("tr");
 
   carElements.forEach((car) => {
     const td = document.createElement("td");
-    if (car === image) {
+    if (car.type === "image") {
       const carImage = document.createElement("img");
       td.appendChild(carImage);
       carImage.style.width = "100px";
-      carImage.src = `${car.value}`;
-    } else if (car === color) {
+      carImage.src = `${car.value.src}`;
+      carImage.alt = `${car.value.alt}`;
+    } else if (car.type === "color") {
       const carColorSquare = document.createElement("div");
       td.appendChild(carColorSquare);
       carColorSquare.style.width = "100px";
@@ -61,17 +92,21 @@ function main() {
       if (carArray.length === 0) {
         const tr = document.createElement("tr");
         const td = document.createElement("td");
+        const thsLength = document.querySelectorAll("table th").length;
 
         td.textContent = "Nenhum carro encontrado";
         tr.appendChild(td);
-        td.setAttribute("colspan", "5");
+        td.setAttribute("colspan", thsLength);
         td.setAttribute("align", "center");
         td.classList.add("noCars");
         table.appendChild(tr);
         return;
       }
 
-      createTableRow(carArray);
+      carArray.forEach((car) => createTableRow(car));
+    })
+    .catch((err) => {
+      ({ error: true, message: err.message });
     });
 }
 
