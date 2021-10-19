b4202e6ad8f205f32ffc140c5ba941b8ff0dbef4
William Firmino <williamwf07@gmail.com>
2021-10-18 21:42:25 -0300

    Exercício 5 - Tipando os arquivos utilizando Typescript

---
+++ dia5/app/dia5/src/http.ts
@@ -0,0 +1,36 @@
+// type Headers = {
+//   [key: string]: string
+// }
+
+// type Options = {
+//   method: string,
+//   headers: Headers,
+//   body: string
+// }
+
+// type DataPost = {
+//   image: string,
+//   brandModel: string,
+//   year: string,
+//   plate: string,
+//   color: string
+// }
+
+const request = (url: string, options?: RequestInit) =>
+  fetch(url, options)
+    .then(r => r.json())
+    .catch(e => ({ error: true, message: e.message }))
+
+type Methods = 'POST' | 'DELETE'
+
+const createRequest = (method: Methods) => <T>(url: string, data: T) => request(url, {
+  method,
+  headers: {
+    'content-type': 'application/json',
+  },
+  body: JSON.stringify(data)
+})
+
+export const get = request
+export const post = createRequest('POST')
+export const del = createRequest('DELETE')
